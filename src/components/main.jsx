ca73811cae5a88fa3ea758199a15c77e97751ac3
downhillthursday <adhirajmohanta@gmail.com>
2025-06-12 17:43:13 +0530

    completed paragraph

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -4,6 +4,8 @@ export default function Main() {
             <h1 className="font-inter text-white text-center text-xl font-bold mx-auto">
                 Stressing over what gear to get to begin your college life? We've got you covered!
             </h1>
+            <h2>Simply pick your Major, set your budget, and we’ll recommend a tailored starter kit that actually makes sense. </h2>
+            <h3>No fluff, just the good stuff.</h3>
         </div>
     )
 }
