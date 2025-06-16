91dd06cf28f87e146ff42f3136cfc25509164019
downhillthursday <adhirajmohanta@gmail.com>
2025-06-16 17:42:39 +0530

    added form

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,5 +1,5 @@
 export default function Main() {
-    return (
+    return (<>
         <div className="lg:max-w-[70%] mx-auto flex flex-col gap-y-6" >
             <h1 className="font-inter text-white text-center text-5xl font-bold mx-auto">
                 Stressing over what gear to get to begin your college life? We've got you covered!
@@ -7,5 +7,9 @@ export default function Main() {
             <h2 className="font-inter text-white text-center text-4xl font-bold mx-auto">Simply pick your Major, set your budget, and we’ll recommend a tailored starter kit that actually makes sense. </h2>
             <h3 className="font-inter text-white text-center text-3xl font-bold mx-auto">No fluff, just the good stuff.</h3>
         </div>
+        <form  name="PickProfession" className="text-2xl w-[60%] mx-auto p-6 rounded-2xl shadow-lg bg-white">
+            <h2 className="w-1/2 mx-auto" >PICK YOUR PROFESSION:</h2>
+            </form>
+            </>
     )
 }
