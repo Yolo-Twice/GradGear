404c7c031365766f0a0689702949fbc3e876d62f
downhillthursday <adhirajmohanta@gmail.com>
2025-06-25 17:11:13 +0530

    added link to the icon

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,15 +1,16 @@
 export default function Header() {
     return (
         <header className="flex items-center justify-center gap-4 shadow-lg bg-[#7289da] px-6 py-3 h-[80px]">
-            <img
-                src="gearpng.parspng.com-7.png"
-                alt="GradGearIcon"
-                className="h-[60px] w-[60px] object-contain transition-transform duration-300 hover:scale-105"
-            />
-            <h1 className="text-[40px] text-[#FAF9F6] font-extrabold tracking-wide font-sans drop-shadow-md">
-                GradGear
-            </h1>
-            
+            <a href="/" className="flex items-center gap-4 h-[60px]">
+                <img
+                    src="gearpng.parspng.com-7.png"
+                    alt="GradGearIcon"
+                    className="h-full w-[60px] object-contain transition-transform duration-300 hover:scale-105"
+                />
+                <h1 className="text-[40px] text-[#FAF9F6] font-extrabold tracking-wide font-sans drop-shadow-md">
+                    GradGear
+                </h1>
+            </a>
         </header>
     )
 }
