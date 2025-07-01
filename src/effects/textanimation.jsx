c3c02d7a2470a6ce3521e20a1c938a3cba64c30e
downhillthursday <adhirajmohanta@gmail.com>
2025-07-01 17:24:36 +0530

    p3ijwe

---
+++ src/effects/textanimation.jsx
@@ -0,0 +1,9 @@
+import { TextAnimate } from "@/components/magicui/text-animate";
+
+export function TextAnimateDemo6() {
+  return (
+    <TextAnimate animation="slideLeft" by="character">
+      Slide left by character
+    </TextAnimate>
+  );
+}
